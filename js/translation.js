8d8a214a5c962f86ce4045d1cf1d834dcd1cbcdf
Magnus Fredlundh <Magnus@Magnus-MacBook.local>
2008-06-04 17:13:33 +0200

    IE gnäller inte nu

---
+++ js/translation.js
@@ -0,0 +1,18 @@
+fapp.translation = {
+/* 
+sv: {
+	message: 'Obesvarade frågor har markerats med gul bakgrund.';
+	message_obligatory: 'Det finns obligatoriska frågor som ej besvarats.',
+	message_few: 'Var god besvara',
+	question: 'fråga',
+	status: 'Status',
+	loading: 'Laddar...' 
+	saved: 'Sparades kl.' },
+*/
+no: {
+	message: 'Meddejling', 
+	status: 'Status' },
+fr: {
+	message: 'Message',
+	status: 'vet inte' }
+}
