41e0b0edc8f078264f9733a60a109e0d3fe30ed4
Magnus Fredlundh <Magnus@Magnus-MacBook.local>
2008-06-11 17:15:20 +0200

    Ändrat kod i form_edit, fungerar fortfarande inte i IE

--- js/form_edit.js
+++ js/form_edit.js
@@ -1,41 +1,27 @@
-function setActiveStyleSheet(title) {
-	var i, a, main;
-	for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-		if(a.getAttribute("rel") != null) {
-			if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
-				a.disabled = true;
-				if(a.getAttribute("title") == title) a.disabled = false;
+function switchStyleSheet(name) {
+	if(!window.ie) {
+		var linkTag, linkTitle = "t_" + name;
+		var linksArray = document.getElementsByTagName("link");
+
+		for(var linkNum=0; linkNum<linksArray.length; linkNum++) {
+			linkTag = linksArray[linkNum];
+			if(linkTag.getAttribute("rel") != null) {
+				if(linkTag.getAttribute("rel").match(/^sty|^alt/i)) {
+
+					if (linkTag.getAttribute("title") == linkTitle) {
+						linkTag.disabled = false;
+					} else if (linkTag.getAttribute("title")) {
+						linkTag.disabled = true;
+					}
+
+				}
 			}
 		}
 	}
+	else {
+		alert("Hej IE : " + name);
+		if (document.styleSheets[0].href != null) {
+			document.styleSheets[0].href = "style/t_" + name + ".css";
+		}
+	}
 }
-/* Används vid sparning */
-function getActiveStyleSheet() {
-  var i, a;
-  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) return a.getAttribute("title");
-  }
-  return null;
-}
-
-function getPreferredStyleSheet() {
-  var i, a;
-  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-    if(a.getAttribute("rel").indexOf("style") != -1
-       && a.getAttribute("rel").indexOf("alt") == -1
-       && a.getAttribute("title")
-       ) return a.getAttribute("title");
-  }
-  return null;
-}
-
-/* Sparar vilken css-fil som används */
-window.onunload = function(e) {
-  //var title = getActiveStyleSheet();
-  //createCookie("style", title, 365);
-}
-
-addLoadEvent(function() {
-	var title = "default";
-	setActiveStyleSheet(title);
-});
